15c2c0880c96fe5786a5626d0eaa8bab79eb01bd
Daud gul <codrdaud@gmail.com>
2024-09-03 10:06:03 +0530

    Fixed green color in button

--- src/components/FeedbackButton.jsx
+++ src/components/FeedbackButton.jsx
@@ -4,11 +4,16 @@ import React from "react";
 function FeedbackButton({ handleOpen }) {
   return (
     <Box>
-      <Button variant="contained" color="secondary" onClick={handleOpen}>
+      <Button
+        variant="contained"
+        color="secondary"
+        sx={{ color: "#34c94b" }}
+        onClick={handleOpen}
+      >
         {" "}
         <span
           style={{
-            color: "#34c94b",
+            // color: "#34c94b",
             textTransform: "capitalize",
             fontWeight: "600",
             fontSize: "18px",
